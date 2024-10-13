85ebb45a879243bb7b7c1f40dd53128afb20855c
efraimdahl <efraimdahl@uchicago.edu>
2024-10-13 13:34:49 +0200

    succsffully getting wavesurf regions to work

--- static/js/index.js
+++ static/js/index.js
@@ -19,7 +19,6 @@ $(document).ready(function() {
 	
     bulmaSlider.attach();
 
-	let wavesurferplaying = false
 	const wavesurfer = WaveSurfer.create({
 		container: '#waveform',
 		waveColor: '#4F4A85',
@@ -38,7 +37,66 @@ $(document).ready(function() {
 	wavesurfer.on('interaction', () => {
 		wavesurfer.playPause()
 	  })
-  
 
+	
+	const wavesurfer_horn = WaveSurfer.create({
+		container: '#waveform_horn',
+		waveColor: '#ffd700',
+		progressColor: '#a78f04',
+		url: './static/audio/Horn.mp3',
+		plugins: [
+			WaveSurfer.Regions.create(),
+			WaveSurfer.Hover.create({
+				lineColor: '#000000',
+				lineWidth: 2,
+				labelColor: '#fff',
+				labelSize: '11px',
+			  }),
+			
+
+		]
+	})
+	wavesurfer_horn.on('ready',() =>{
+	wavesurfer_horn.plugins[0].addRegion({
+		content: "Target: Efraim Singing",
+		start: 0, // start time in seconds
+		end: 6, // end time in seconds
+		color: '#ffd70031', // region color
+		drag: false,
+		resize: false,
+
+	});
+	wavesurfer_horn.plugins[0].addRegion({
+		content: "Reference: French Horn playing ode to joy",
+		start: 6.2, // start time in seconds
+		end: 12, // end time in seconds
+		color: '#a78f0431', // region color
+		drag: false,
+		resize: false,
+
+	});
+	wavesurfer_horn.plugins[0].addRegion({
+		content: "Output: Strange Horns",
+		start: 12.2, // start time in seconds
+		end: 20, // end time in seconds
+		color: '#ffd70031', // region color
+		drag: false,
+		resize: false,
+
+	});
+	})
+	wavesurfer_horn.on('interaction', () => {
+		wavesurfer_horn.playPause();
+		
+	  })
+  
+	  /*
+	 
+	  
+		
+		
+			  */
+
+			  
 
 })
